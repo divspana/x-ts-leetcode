591c39dfc81fa4f08578f629fe02f2793a65c8a6
yang <xiangyang@yidayuntu.cn>
2024-02-28 21:28:28 +0800

    增加dayjs,打印今日时间.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+import dayjs = require("dayjs");
+
 console.log('<================算法练习库，每日一题===============>')
-console.log('今日:',new Date().toUTCString())
-
-
+console.log('今日:',dayjs().format('YYYY-MM-DD'))
 // 引入今日算法文件
