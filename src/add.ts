1e6eb7f271b39f9c7f248c56de290ea6350c054e
yang <xiangyang@yidayuntu.cn>
2024-02-29 18:53:09 +0800

    20题，有效的括号，栈的考察

--- src/add.ts
+++
@@ -1,6 +0,0 @@
-console.log('wwww.xiangyang')
-
-export function add(a:number, b:number) {
-    console.log(a, b);
-
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import dayjs = require("dayjs");
-
+const dayjs=require('dayjs')
 console.log('<================算法练习库，每日一题===============>')
 console.log('今日:',dayjs().format('YYYY-MM-DD'))
 // 引入今日算法文件
+
+import {test} from './20'
+
+test()
+
+
